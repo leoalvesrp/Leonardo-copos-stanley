f39ebbdaafa73a9fa49810c2c3697eb575290063
Leonardo Alves <113029288+leoalvesrp@users.noreply.github.com>
2023-06-27 08:34:05 -0300

    pagina produto  e ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 import Image from'next/image'
 import copo1 from'../../public/copos/copo-1.png'
 import copo2 from'../../public/copos/copo-2.png'
+import copo3 from'../../public/copos/copo-3.webp'
+
 import {ShoppingCart} from'lucide-react'
 
 export default function Home() {
   return (
     <main className='grid grid-cols-3 gap-[2rem] max-w-[1180px] m-auto mt-[3rem]'>
       <div className='flex-col justify-center shadow-sm'>
-        <Image className='object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md' src={copo1} alt=''/>
+        <Image className='w-full object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md h-[300px]' src={copo1} alt=''/>
         <footer className='flex-col justify-center bg-gray-800 text-center font-sans rounded-md'>
           <h1 className='text-[2rem]'>Copo Térmico Stanley</h1>
           <span>473ml</span>
           <p className='text-[1.5rem]'>Branco</p>
-          <span className='block text-[1.5rem]'>R$ 70,00</span>
+          <span className='block text-[1.5rem] text-green-400'>R$ 70,00</span>
           <button className='bg-blue-900 h-[3.5rem] w-[15rem]  mx-auto mt-[0.5rem] mb-[1rem]  rounded-md border-0 font-bold text-[1.5rem] hover:bg-blue-950 transition-colors duration-300 '>
             COMPRAR
             <ShoppingCart size={24} className='inline align-middle ml-[10px] mb-[5px]'/>
@@ -20,12 +22,12 @@ export default function Home() {
         </footer>
       </div>
       <div className='flex-col justify-center shadow-sm'>
-        <Image className='object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md' src={copo1} alt=''/>
+        <Image className='w-full object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md h-[300px]' src={copo2} alt=''/>
         <footer className='flex-col justify-center bg-gray-800 text-center font-sans rounded-md'>
           <h1 className='text-[2rem]'>Copo Térmico Stanley</h1>
           <span>473ml</span>
           <p className='text-[1.5rem]'>Branco</p>
-          <span className='block text-[1.5rem]'>R$ 70,00</span>
+          <span className='block text-[1.5rem] text-green-400'>R$ 70,00</span>
           <button className='bg-blue-900 h-[3.5rem] w-[15rem]  mx-auto mt-[0.5rem] mb-[1rem]  rounded-md border-0 font-bold text-[1.5rem] hover:bg-blue-950 transition-colors duration-300 '>
             COMPRAR
             <ShoppingCart size={24} className='inline align-middle ml-[10px] mb-[5px]'/>
@@ -33,57 +35,19 @@ export default function Home() {
         </footer>
       </div>
       <div className='flex-col justify-center shadow-sm'>
-        <Image className='object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md' src={copo1} alt=''/>
+        <Image className='w-full object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md h-[300px]' src={copo3} alt=''/>
         <footer className='flex-col justify-center bg-gray-800 text-center font-sans rounded-md'>
           <h1 className='text-[2rem]'>Copo Térmico Stanley</h1>
           <span>473ml</span>
           <p className='text-[1.5rem]'>Branco</p>
-          <span className='block text-[1.5rem]'>R$ 70,00</span>
+          <span className='block text-[1.5rem] text-green-400'>R$ 70,00</span>
           <button className='bg-blue-900 h-[3.5rem] w-[15rem]  mx-auto mt-[0.5rem] mb-[1rem]  rounded-md border-0 font-bold text-[1.5rem] hover:bg-blue-950 transition-colors duration-300 '>
             COMPRAR
             <ShoppingCart size={24} className='inline align-middle ml-[10px] mb-[5px]'/>
             </button>
         </footer>
       </div>
-      <div className='flex-col justify-center shadow-sm'>
-        <Image className='object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md' src={copo1} alt=''/>
-        <footer className='flex-col justify-center bg-gray-800 text-center font-sans rounded-md'>
-          <h1 className='text-[2rem]'>Copo Térmico Stanley</h1>
-          <span>473ml</span>
-          <p className='text-[1.5rem]'>Branco</p>
-          <span className='block text-[1.5rem]'>R$ 70,00</span>
-          <button className='bg-blue-900 h-[3.5rem] w-[15rem]  mx-auto mt-[0.5rem] mb-[1rem]  rounded-md border-0 font-bold text-[1.5rem] hover:bg-blue-950 transition-colors duration-300 '>
-            COMPRAR
-            <ShoppingCart size={24} className='inline align-middle ml-[10px] mb-[5px]'/>
-            </button>
-        </footer>
-      </div>
-      <div className='flex-col justify-center shadow-sm'>
-        <Image className='object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md' src={copo1} alt=''/>
-        <footer className='flex-col justify-center bg-gray-800 text-center font-sans rounded-md'>
-          <h1 className='text-[2rem]'>Copo Térmico Stanley</h1>
-          <span>473ml</span>
-          <p className='text-[1.5rem]'>Branco</p>
-          <span className='block text-[1.5rem]'>R$ 70,00</span>
-          <button className='bg-blue-900 h-[3.5rem] w-[15rem]  mx-auto mt-[0.5rem] mb-[1rem]  rounded-md border-0 font-bold text-[1.5rem] hover:bg-blue-950 transition-colors duration-300 '>
-            COMPRAR
-            <ShoppingCart size={24} className='inline align-middle ml-[10px] mb-[5px]'/>
-            </button>
-        </footer>
-      </div>
-      <div className='flex-col justify-center shadow-sm'>
-        <Image className='object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md' src={copo1} alt=''/>
-        <footer className='flex-col justify-center bg-gray-800 text-center font-sans rounded-md'>
-          <h1 className='text-[2rem]'>Copo Térmico Stanley</h1>
-          <span>473ml</span>
-          <p className='text-[1.5rem]'>Branco</p>
-          <span className='block text-[1.5rem]'>R$ 70,00</span>
-          <button className='bg-blue-900 h-[3.5rem] w-[15rem]  mx-auto mt-[0.5rem] mb-[1rem]  rounded-md border-0 font-bold text-[1.5rem] hover:bg-blue-950 transition-colors duration-300 '>
-            COMPRAR
-            <ShoppingCart size={24} className='inline align-middle ml-[10px] mb-[5px]'/>
-            </button>
-        </footer>
-      </div>
+      
       
       
       
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,10 +1,24 @@
-import Link from "next/link";
+import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import copo1 from'../../../public/copos/copo-1.png'
 
 export default function Product() {
   return (
-    <div>
-      <h1 className='text-2xl font-semibold'>Product</h1>
-      <Link href={`/`}>Home</Link>
-    </div>
+    <main className='grid grid-cols-2 gap-[2rem] max-w-[1000px] m-auto mt-[3rem]'>
+      <div className='flex justify-center shadow-sm max-w-full gap-[5rem]'>
+        <Image className='object-cover bg-gradient-to-r from-indigo-500 to-green-400 rounded-t-md w-full' src={copo1} alt=''/>
+      </div>
+        <aside className='flex-col justify-center bg-gray-800 text-center font-sans rounded-md'>
+          <h1 className='text-[2rem]'>Copo Térmico Stanley</h1>
+          <span className='block text-[1.5rem] text-green-400'>R$ 70,00</span>
+          <p className="text-[1rem]">O Copo Stanley é um copo térmico durável e icônico feito de aço inoxidável de alta qualidade. Ele é conhecido por sua resistência e capacidade de manter bebidas quentes ou frias por longos períodos. Com uma tampa hermética, é perfeito para viagens e atividades ao ar livre. Possui alça para facilitar o transporte, boca larga para beber confortavelmente e é resistente a arranhões. Além disso, promove a sustentabilidade ao reduzir o uso de copos descartáveis. O Copo Stanley é o companheiro ideal para manter suas bebidas na temperatura certa em qualquer lugar.</p>
+          <span>473ml</span>
+          <p className='text-[1.5rem]'>Branco</p>
+          <button className='bg-blue-900 h-[4.5rem] w-[25rem]  mx-auto mt-[1rem] mb-[1rem]  rounded-md border-0 font-bold text-[1.5rem] hover:bg-blue-950 transition-colors duration-300 '>
+            ADICIONAR AO CARRINHO
+            <ShoppingCart size={24} className='inline align-middle ml-[10px] mb-[5px]'/>
+            </button>
+        </aside>
+      </main>
   )
 }
